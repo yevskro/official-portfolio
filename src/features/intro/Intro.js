91d3c4b5c7553bf41007e127ad4c12dda0a6bb08
Yevgeniy Skroznikov <yevskro@gmail.com>
2020-09-22 15:08:07 -0400

    feat(app): setup boilerplate

---
+++ src/features/intro/Intro.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Intro = (props) => {
+    return 'intro'
+}
+
+export default Intro;
---
+++ src/features/project/Project.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Project = (props) => {
+    return 'project';
+}
+
+export default Project;
