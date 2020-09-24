ee6338231b18bf96fa042e84f6374c54401c4599
Yevgeniy Skroznikov <yevskro@gmail.com>
2020-09-24 12:13:44 -0400

    feat(Project): using figure semmantic tag

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,16 +1,31 @@
 import React from 'react';
 import styled from 'styled-components';
+import Window from './Window';
 
 const Project = (props) => <Container>
-    return 'project';
+  <figure>
+    <Logo/>
+    <Header>Project1</Header>
+    <Caption>Durran</Caption>
+    <Window/>
+  </figure>
 </Container>
 
 const Logo = styled.img``;
-const Header = styled.h1``;
-const Summary = styled.p``;
-
-const Container = styled.section`
-    margin-left: 23.1%;
-    border-left: 1px solid RGB(227,231,235);
+const Header = styled.h1`
+  font-size: 2em;
 `;
+const Caption = styled.figcaption`
+  font-family: 'Source Code Pro', monospace;
+  margin-bottom: 3em;
+  display: inline-block;
+  width: 700px;
+`;
+
+const Container = styled.div`
+  margin-left: 23.1%;
+  border-left: 1px solid RGB(227,231,235);
+  padding-bottom: 2em;
+`
+
 export default Project;
