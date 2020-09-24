2e6d7d33c04edda97e0e84a225813a75e26190ad
Yevgeniy Skroznikov <yevskro@gmail.com>
2020-09-24 11:55:40 -0400

    fix(Header): lasting the animation at 0.6s

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,40 +1,148 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 
-const Header = () => <Container>
-    <PrimaryButton href="https://github.com/yevskro">Github</PrimaryButton>
-    <SecondaryButton href="mailto:yevskro@gmail.com?Subject=Hello Yevgeniy">Hire Me</SecondaryButton>
-</Container>;
+const Header = () => <StyledHeader>
+  <Hi>hi! I am...</Hi>
+  <FlashName>
+    <Name>Yevgeniy Skroznikov</Name>
+    <FlashJob>
+      <JobDescription>
+        I am a <NeonText>Software Engineer</NeonText> with skills across the stack.
+      </JobDescription>
+      <FlashSkills>
+        <Skills>
+          <ReactSkill>React</ReactSkill>
+          <ReduxSkill>Redux</ReduxSkill>
+          <ExpressSkill>Express</ExpressSkill>
+          <NodeSkill>Node</NodeSkill>
+          <DbSkill>SQL/NoSQL</DbSkill>
+        </Skills>
+        <FlashMessage>
+          <Message>I've worked with <NeonText>TypeScript</NeonText>, <NeonText>Test Driven Development</NeonText>, <NeonText>CI/CD</NeonText>, <NeonText>Docker</NeonText>, <NeonText>AWS</NeonText>, and the <NeonText>MVC</NeonText> architecture. I am the maintainer of <NeonText>Nautilus</NeonText> an <NeonText>open source</NeonText> product that visualizes and deploys <NeonText>docker-compose</NeonText> files within an <NeonText>Electron</NeonText> application.</Message>
+          <FlashEmpty/>
+        </FlashMessage>
+      </FlashSkills>
+    </FlashJob>
+  </FlashName>
+</StyledHeader>
 
-const Container = styled.header`
+const becomeVisible = keyframes`
+    0%      { opacity: 0; }
+    100%    { opacity: 1; }
+}`;
+
+const Flash = styled.section`
+  display: block;
+  overflow: hidden;
+  height: 100%;
+  margin-top: 25px;
+  padding-left: 55px;
+  border-left: 1px solid RGB(63,69,73);
+  opacity: 0;
+  animation: 0.6s ease-in 0s 1 ${becomeVisible} forwards;
+`;
+
+const FlashEmpty = styled(Flash)`
+  animation-delay: 2.2s;
+`;
+const FlashMessage = styled(Flash)`
+  animation-delay: 1.4s;
+`;
+const FlashSkills = styled(Flash)`
+  animation-delay: 1s;
+`;
+
+const Skills = styled.ol`
   display: flex;
-  justify-content: flex-end;
-  padding-right: 8%;
-  padding-left: 8%;
+  padding: 0;
+  justify-content: space-around;
+  list-style: none;
 `;
 
-const Button = styled.a`
-  display: flex;
-  width: 143px;
-  height: 53px;
-  justify-content: center;
-  align-items: center;
-  border: 1px solid RGB(50,54,58);
-  border-radius: 2px;
-  font-size: 1em;
-  margin-top: 1%;
-  margin-left: 20px;
-  margin-right: 20px;
-  cursor: pointer;
-  background-color: RGB(39,43,46);
+const Skill = styled.li`
+    background-color: RGB(38, 43, 46);
+    font-size: 0.85em;
+    padding: 2% 2% 2% 2%;
+    border-radius: 2px;
+    width: 100px;
+    height: 55px;
+    text-align: center;
+    vertical-align: center;
+    line-height: 55px;
+    color: RGB(224,238,246);
+    font-size: 1.2em;
+    opacity: 0;
+    animation: 0.6s ease-in 0s 1 ${becomeVisible} forwards;
 `;
 
-const PrimaryButton = styled(Button)`
-    color: RGB(234,191,16);
+const ReactSkill = styled(Skill)`
+  animation-delay: 1.2s;
 `;
 
-const SecondaryButton = styled(Button)`
-    color: RGB(255,255,255);
+const ReduxSkill = styled(Skill)`
+  animation-delay: 1.4s;
+`;
+
+const ExpressSkill = styled(Skill)`
+  animation-delay: 1.6s;
+`;
+
+const NodeSkill = styled(Skill)`
+  animation-delay: 1.8s;
+`;
+
+const DbSkill = styled(Skill)`
+  animation-delay: 2s;
+`;
+
+const FlashJob = styled(Flash)`
+  animation-delay: 0.6s;
+`;
+const Message = styled.span`
+  color: RGB(117,133,143);
+  font-size: 1.1em;
+  opacity: 0;
+  animation: 0.6s ease-in 0s 1 ${becomeVisible} forwards;
+  animation-delay: 2.2s;
+`;
+const JobDescription = styled.span`
+  color: RGB(117,133,143);
+  font-size: 1.1em;
+  opacity: 0;
+  animation: 0.6s ease-in 0s 1 ${becomeVisible} forwards;
+  animation-delay: 1s;
+`;
+
+const NeonText = styled.span`
+  color: RGB(80, 251,156);
+  font-size: 1.1em
+`
+const Name = styled.span`
+  color: RGB(224,238,246);
+  font-size: 4em;
+  opacity: 0;
+  animation: 0.7s ease-in 0s 1 ${becomeVisible} forwards;
+  animation-delay: 0.6s;
+`;
+const Hi = styled.span`
+  color: RGB(224,238,246);
+  font-size: 1.5em;
+  opacity: 0;
+  animation: 0.6s ease-in 0s 1 ${becomeVisible} forwards;
+`;
+const FlashName = styled(Flash)`
+  animation: 0.6s ease-in 0s 1 ${becomeVisible} forwards;
+`;
+
+// full animation 0.5s, 0.15 delay for next container, and delay 10s for next message
+
+const StyledHeader = styled.header`
+  display: block;
+  height: 560px;
+  width: 860px;
+  margin-left: 23.1%;
+  margin-top: 80px;
+  overflow: hidden;
 `;
 
 export default Header;
