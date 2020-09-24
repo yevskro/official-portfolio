d5dbfaae44f887a6524b27ca2fce9635a2f3d54f
Yevgeniy Skroznikov <yevskro@gmail.com>
2020-09-24 12:13:03 -0400

    fix: changed previous header to nav

---
+++ src/components/Nav.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Nav = () => <StyledNav>
+    <PrimaryButton href="https://github.com/yevskro">Github</PrimaryButton>
+    <SecondaryButton href="mailto:yevskro@gmail.com?Subject=Hello Yevgeniy">Hire Me</SecondaryButton>
+</StyledNav>;
+
+const StyledNav = styled.nav`
+  display: flex;
+  justify-content: flex-end;
+  padding-right: 8%;
+  padding-left: 8%;
+`;
+
+const Button = styled.a`
+  display: flex;
+  width: 143px;
+  height: 53px;
+  justify-content: center;
+  align-items: center;
+  border: 1px solid RGB(50,54,58);
+  border-radius: 2px;
+  font-size: 1em;
+  margin-top: 1%;
+  margin-left: 20px;
+  margin-right: 20px;
+  cursor: pointer;
+  background-color: RGB(39,43,46);
+`;
+
+const PrimaryButton = styled(Button)`
+    color: RGB(234,191,16);
+`;
+
+const SecondaryButton = styled(Button)`
+    color: RGB(255,255,255);
+`;
+
+export default Nav;
